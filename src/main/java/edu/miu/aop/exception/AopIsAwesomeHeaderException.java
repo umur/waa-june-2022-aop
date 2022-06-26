62d631e9552dd182d9eef1c092a2807819164b62
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-06-26 14:23:42 -0500

    Add AwesomeHeaderValidation

---
+++ src/main/java/edu/miu/aop/exception/AopIsAwesomeHeaderException.java
@@ -0,0 +1,9 @@
+package edu.miu.aop.exception;
+
+public class AopIsAwesomeHeaderException extends RuntimeException {
+
+    public AopIsAwesomeHeaderException(String message) {
+        super(message);
+    }
+
+}
