925287ff89ca1fd11730f3a55f2d7b6c298ac273
AKay-Cee <95042223+AKay-Cee@users.noreply.github.com>
2022-06-26 17:38:30 -0500

    controller, aspect, service, dto added

--- lab5/src/main/java/com/example/lab5/dto/ActivityDto.java
+++ lab5/src/main/java/com/example/lab5/dto/ActivityDto.java
@@ -1,10 +1,24 @@
 package com.example.lab5.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityDto {
     private int id;
     private Date date;
     private String operation;
-    private String duration;
+    private long duration;
+
+    public ActivityDto(Date date, String operation, long duration){
+        this.date=date;
+        this.operation=operation;
+        this.duration=duration;
+    }
 }
---
+++ lab5/src/main/java/com/example/lab5/exception/AopIsAwesomeHeaderException.java
@@ -0,0 +1,7 @@
+package com.example.lab5.exception;
+
+public class AopIsAwesomeHeaderException extends Exception{
+    public AopIsAwesomeHeaderException(String s){
+        super(s);
+    }
+}
