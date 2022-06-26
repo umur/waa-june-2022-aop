c1dea886f7b638a4858b41bc38140bcb42543f30
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2022-06-26 05:52:48 -0500

    implementation of ExecutionTime annotation and Aspect

--- src/main/java/edu/miu/cs545/aop/aspect/ExecutionTimeAspect.java
+++ src/main/java/edu/miu/cs545/aop/aspect/ExecutionTimeAspect.java
@@ -1,4 +1,18 @@
 package edu.miu.cs545.aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
 public class ExecutionTimeAspect {
+
+    @Around("@annotation(edu.miu.cs545.aop.aspect.annotation.ExecutionTime)")
+    public Object calculateExecutationTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long start=System.nanoTime();
+        var result=proceedingJoinPoint.proceed();
+        long finish = System.nanoTime();
+        System.out.println(proceedingJoinPoint.getSignature().getName() +"takes ns" + (finish-start));
+        return result;
+    }
 }
