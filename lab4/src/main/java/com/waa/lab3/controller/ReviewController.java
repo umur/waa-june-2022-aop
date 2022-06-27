038e08e32e55a8739d4c11bb45c66ea7ebcc5d20
muztaba <hasanat.muztaba@gmail.com>
2022-06-26 21:35:12 -0500

    ExecutionTIme

---
+++ lab4/src/main/java/com/waa/lab3/controller/ReviewController.java
@@ -0,0 +1,24 @@
+package com.waa.lab3.controller;
+
+import com.waa.lab3.aspect.annotation.ExecutionTime;
+import com.waa.lab3.entity.Review;
+import com.waa.lab3.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/review")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/search")
+    @ExecutionTime
+    public Review findByProductId(@RequestParam Long productId) {
+        return reviewService.findReviewByProductId(productId);
+    }
+}
---
+++ lab4/src/main/java/com/waa/lab3/repo/ActivityLogRepository.java
@@ -0,0 +1,9 @@
+package com.waa.lab3.repo;
+
+import com.waa.lab3.entity.ActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
+}
